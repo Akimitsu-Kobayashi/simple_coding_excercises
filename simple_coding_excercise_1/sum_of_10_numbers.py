c82cfcf6d2d0234e62c5a1fd711537995c371125
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-11 17:23:37 +0800

    sum of 10 num psuedocode

---
+++ simple_coding_excercise_1/sum_of_10_numbers.py
@@ -0,0 +1,7 @@
+#Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
+
+#loop 10 times
+    #ask user for a number
+    #add number to the sum of all numbers
+
+#print the sum
