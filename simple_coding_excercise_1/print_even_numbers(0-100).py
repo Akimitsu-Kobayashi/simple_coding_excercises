1729fb36f19c82569d87be609c5598f3339d75d3
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-11 19:21:05 +0800

    even numbers from 1 - 100

--- simple_coding_excercise_1/print_even_numbers(0-100).py
+++ simple_coding_excercise_1/print_even_numbers(0-100).py
@@ -1,4 +1,7 @@
 #Prog09: Create a program that print all the even numbers starting from 0 to 100. (Use for loop)
 
 #loop 100 times
+for number in range(1,101,1):
     # if a number is even print the number
+    if number%2 == 0:
+        print(number)  
