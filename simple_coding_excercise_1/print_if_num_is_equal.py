e7514a574e130e62d5e4e5b1c4078f5aea48ab6c
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-11 16:40:37 +0800

    print Equal when the 2 inputs are the same

---
+++ simple_coding_excercise_1/print_if_num_is_equal.py
@@ -0,0 +1,9 @@
+#Prog02: Create a program that ask user to input 2 numbers. Print "Equal" when the numbers are the same.
+
+# get 2 numbers from user
+num1 = int(input("num 1: "))
+num2 = int(input("num 2: "))
+
+ # if the numbers are equal then print "Equal"
+if num1 == num2:
+    print("Equal")
