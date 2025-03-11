df45fa41ef3a1a23c428e476d8c0fd671d32cd32
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-11 16:56:11 +0800

    product of two number

---
+++ simple_coding_excercise_1/product_of_two_num.py
@@ -0,0 +1,11 @@
+#Prog04: Create a program that ask user to input 2 numbers. Print the product of the two numbers.
+
+#get 2 number from user
+num_1 = int(input("number 1: "))
+num_2 = int(input("number 2: "))
+
+#multiply the 2 numbers
+product = num_1*num_2
+
+#print the product
+print(product)
