7aa9d0e6f8cdd38cd96492e4358b62b1541fc928
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-11 16:29:58 +0800

    print the bigger num

--- simple_coding_excercise_1/print_bigger_number.py
+++ simple_coding_excercise_1/print_bigger_number.py
@@ -1,5 +1,14 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the bigger number.
 
 # get 2 number from user 
+num_1 = int(input("number1: "))
+num_2 = int(input("number2: "))
 # compare 2 numbers usin > or <
+if(num_1>num_2):
     # print the bigger number
+    print(str(num_1)+" is the biggest num")
+else:
+    # print the bigger number
+    print(str(num_2)+" is the biggest num")
+
+    
