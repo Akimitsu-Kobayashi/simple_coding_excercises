08da1c290ec781ea77b0c5b705d703968567e5d6
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-11 17:29:43 +0800

    doesnt increase number iteration and doesnt end

--- simple_coding_excercise_1/sum_of_10_numbers.py
+++ simple_coding_excercise_1/sum_of_10_numbers.py
@@ -1,7 +1,13 @@
 #Prog07: Create a program that ask user to input 10 numbers. Print the sum of all the numbers.
 
 #loop 10 times
+i = 0
+sum_of_ten_num = 0
+while i < 10:
     #ask user for a number
+    input_num = int(input("number"+ str(i) +":"))
     #add number to the sum of all numbers
+    sum_of_ten_num += input_num
 
 #print the sum
+print(sum_of_ten_num)
