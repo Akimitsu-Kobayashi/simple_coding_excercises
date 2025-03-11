c1601c9fd66406df75cd896281b067ba79a7b0e4
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-11 17:20:14 +0800

    number exponentiation

---
+++ simple_coding_excercise_1/number_exponentiation.py
@@ -0,0 +1,11 @@
+#Prog06: Create a program that ask user to input 2 numbers. Print the result when the first number is raised to the second number.
+
+#get 2 number from user
+num_1 = int(input("number 1: "))
+num_2 = int(input("number 2: "))
+
+#raise the first number by the second number
+resulting_value = num_1**num_2
+
+#print the result
+print(resulting_value)
