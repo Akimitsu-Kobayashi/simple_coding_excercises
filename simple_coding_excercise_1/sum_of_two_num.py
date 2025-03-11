0137ece35d3c19cdb0402523fb3ff9f6461b02c4
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-11 16:48:52 +0800

    adds two numbers

--- simple_coding_excercise_1/sum_of_two_num.py
+++ simple_coding_excercise_1/sum_of_two_num.py
@@ -1,5 +1,10 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the sum of the two numbers.
 
 #get 2 numbers from user
+num_1 = int(input("number 1: "))
+num_2 = int(input("number 2: "))
+
 #add 2 numbers
+sum = num_1+num_2
 #print the sum
+print(sum)
