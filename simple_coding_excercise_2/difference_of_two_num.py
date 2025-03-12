23038cac5a352586cd14f8339f7b514c19a3d70f
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-12 23:03:42 +0800

    basic subtraction program

--- simple_coding_excercise_2/difference_of_two_num.py
+++ simple_coding_excercise_2/difference_of_two_num.py
@@ -1,6 +1,12 @@
 #Prog03: Create a program that ask user to input 2 numbers. Print the difference of the two numbers.
 
 #ask the user for 2 numbers
+num_1 = int(input("number 1: "))
+num_2 = int(input("number 2: "))
+
 #subtract num_1 and num_2
-    #print the difference
+difference = num_1-num_2
+
+#print the difference
+print(difference)
 
