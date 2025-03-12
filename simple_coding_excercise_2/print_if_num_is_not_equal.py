6a27245aff7b411f662a9b14461aa2effd93be7c
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-12 22:51:32 +0800

    compares 2 num if not equal

--- simple_coding_excercise_2/print_if_num_is_not_equal.py
+++ simple_coding_excercise_2/print_if_num_is_not_equal.py
@@ -1,5 +1,11 @@
 #Prog02: Create a program that ask user to input 2 numbers. Print "Not Equal" when the numbers are not the same.
 
 #asks the user 2 numbers
+num_1 = int(input("number 1: "))
+num_2 = int(input("number 2: "))
+
 #compare if not equal
+if num_1 != num_2:
+    
     #print "Not Equal" if 2 numbers are not the same value
+    print("Not Equal")
