474c693c163dd3fedb4c349aa8a1986bd87c6fbc
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-13 00:13:49 +0800

    psuedocode

---
+++ simple_coding_excercise_2/first_number_minus_sum_of_num.py
@@ -0,0 +1,8 @@
+#Prog06: Create a program that ask user to input 10 numbers. Print the result of the first number minus all of the remaining numbers.
+
+#loop 10 times
+    #ask user for a number
+    #store first number
+    #sum all numbers after the first number
+
+#print first number minus the sum
