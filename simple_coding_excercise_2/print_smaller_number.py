6205c70869138c4eead5b166804ba01448cf376d
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-12 22:48:32 +0800

    print smaller number

--- simple_coding_excercise_2/print_smaller_number.py
+++ simple_coding_excercise_2/print_smaller_number.py
@@ -1,5 +1,15 @@
 #Prog01: Create a program that ask user to input 2 numbers. Print the smaller number.
     
 #ask user for 2 numbers
+num_1 = int(input("number 1: "))
+num_2 = int(input("number 2: "))
+
 #compare to see the smaller number
+if num_1<num_2:
     #print if it is the smaller number
+    print(str(num_1)+" is the smaller number")
+
+else:
+    #print if it is the smaller number
+    print(str(num_2)+" is the smaller number")
+
