548969b0a774a7d6eda90f19e34311da7ab969c9
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-13 01:43:36 +0800

    numbers inbetween

--- simple_coding_excercise_2/number_inbetween_two_numbers.py
+++ simple_coding_excercise_2/number_inbetween_two_numbers.py
@@ -1,8 +1,16 @@
 #Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
 
 #ask user to input 2 numbers
+num_1 = int(input("number 1: "))
+num_2 = int(input("number 2: "))
 
 #find the lower number to use as the starting range
-#loop based on the range of the 2 numbers
-# print the numbers
+if num_1 < num_2:
+    #loop based on the range of the 2 numbers   
+    for number in range(num_1+1,num_2,1):
+        print(number)
+else:
+    #loop based on the range of the 2 numbers
+    for number in range(num_2+1,num_1,1):
+        print(number)
 
