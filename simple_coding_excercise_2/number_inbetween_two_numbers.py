302abed5cc0f10dfd352168849ec9b610861c355
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-13 01:39:39 +0800

    psuedocode

---
+++ simple_coding_excercise_2/number_inbetween_two_numbers.py
@@ -0,0 +1,8 @@
+#Prog10: Create a program that ask user to input 2 numbers. Print all the numbers between the two numbers.
+
+#ask user to input 2 numbers
+
+#find the lower number to use as the starting range
+#loop based on the range of the 2 numbers
+# print the numbers
+
