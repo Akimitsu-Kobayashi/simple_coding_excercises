ffd2463740079dcdbe23f9aafe429b26082eeec2
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-13 00:33:23 +0800

    changed name

--- simple_coding_excercise_2/even_numbers_0_100.py
+++ simple_coding_excercise_2/even_numbers_0_100.py
@@ -1,5 +1,11 @@
 #Prog08: Create a program that print all the odd numbers starting from 0 to 100. (Use while loop)
 
 #loop until 100
-# differentiate odd numbers
-#print number
+i = 0
+while i <= 100:
+    # differentiate odd numbers
+    if i % 2 != 0:
+        #print number
+        print(i)
+
+
