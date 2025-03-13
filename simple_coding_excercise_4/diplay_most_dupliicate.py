d64eaa27bc2c800a10053c703802741b12b31b5f
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-13 19:04:39 +0800

    Working concept of using a nested list where it groups simillar numbers in asigular list

--- simple_coding_excercise_4/diplay_most_dupliicate.py
+++ simple_coding_excercise_4/diplay_most_dupliicate.py
@@ -1,7 +1,28 @@
 #Prog02: Create a program that ask user to input a number, continue asking until the user input is invalid. Display the number with the most number of duplicate.
 
 inputs = []
+all_nums = []
+duplicates = []
 
 while True:
-    num = int(inputs("Enter a number: "))
+    num = int(input("Enter a number: "))
+
+    for i in range(len(inputs)):
+        if num in inputs[i]:
+            inputs[i].append(num)
     
+    if num not in all_nums:
+            inputs.append([num])
+
+    if num not in duplicates and num in all_nums:
+        duplicates.append(num)
+
+    all_nums.append(num)
+
+
+
+    
+    print(inputs)
+    print(all_nums)
+    print(duplicates)
+
