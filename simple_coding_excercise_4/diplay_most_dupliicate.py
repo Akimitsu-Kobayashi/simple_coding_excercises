8f5d4197c8f5735fabe8b2ebbfc62de369d33cc8
Akimitsu-Kobayashi <akimitsukobayashi07@gmail.com>
2025-03-13 20:57:23 +0800

    working version of displaying most duplicate

--- simple_coding_excercise_4/diplay_most_dupliicate.py
+++ simple_coding_excercise_4/diplay_most_dupliicate.py
@@ -4,32 +4,32 @@ inputs = []
 all_nums = []
 duplicates = []
 most_duplicate = 0
+most_duplicate_counter = 0
+i = 1
 
-
-while True:
+while i>0:
     num = int(input("Enter a number: "))
-    most_duplicate = num
-
+    if i == 1:
+        most_duplicate = num
+    
     for numbers in range(len(inputs)):
-        if num in inputs[numbers]:
+        if num == inputs[numbers][0]:
             inputs[numbers].append(num)
 
-        if len(inputs[numbers])>most_duplicate:
+        if len(inputs[numbers])>len(duplicates):
+             duplicates = inputs[numbers]
              most_duplicate = inputs[numbers][0]
-    
+             
     if num not in all_nums:
             inputs.append([num])
 
     all_nums.append(num)
-    
-    if num not in duplicates and num in all_nums:
-        duplicates.append(num)
+    i += 1
 
-
-
-
-    print(inputs)
-    print(all_nums)
-    print(duplicates)
     print(most_duplicate)
+    print(duplicates)
+    print(inputs)
+
+
+
 
